fffa984857c0d3605ed2bb1a13244e1f9f5dd7f9
MutNation <welpmut9@gmail.com>
2025-11-09 20:51:37 +0300

    Fix: correct seconds calculation formula

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,14 +1,3 @@
-# hours_to_seconds.py
-# This program converts hours into seconds
-
-# Given value
-hours = 2  # Number of hours
-
-# Conversion factor
-seconds_per_hour = 3600
-
-# Calculate total seconds
-seconds = hours * seconds_per_hour
-
-# Print the result
-print(hours, "hour(s) is", seconds, "seconds.")
+hours = 2
+seconds = hours * 3600
+print(f"{hours} hour(s) is {seconds} seconds.")
